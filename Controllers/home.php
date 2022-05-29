5d767064009993f3e16a852058027f7932ff1e90
katayama hayato <>
2022-05-29 12:41:06 +0900

    Twitterclonecomplete

--- Controllers/home.php
+++ Controllers/home.php
@@ -10,17 +10,26 @@ include_once '../util.php';
  
 // ツイートデータ操作モデルを読み込む
 include_once '../Models/tweets.php';
+// フォローデータ操作モデルを読み込む
+include_once '../Models/follows.php';
  
-// ログインしているか
+// ログインチェック
 $user = getUserSession();
 if (!$user) {
     // ログインしていない
-    header('Location:' . HOME_URL . 'Controllers/sign-in.php');
+    header('Location: ' . HOME_URL . 'Controllers/sign-in.php');
     exit;
 }
  
-// 画面表示
+// 自分がフォローしているユーザーID一覧を取得
+$following_user_ids = findFollowingUserIds($user['id']);
+// 自分のツイートも表示するために自分のIDも追加
+$following_user_ids[] = $user['id'];
+ 
+// 表示用の変数
 $view_user = $user;
 // ツイート一覧
-$view_tweets = findTweets($user);
+$view_tweets = findTweets($user, null, $following_user_ids);
+ 
+// 画面表示
 include_once '../Views/home.php';
