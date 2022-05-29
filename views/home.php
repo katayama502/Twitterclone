80e23cf853e2e55f97360520a7a748a99c37525d
katayama hayato <>
2022-05-29 09:11:35 +0900

    Lesson11

--- views/home.php
+++ views/home.php
@@ -1,38 +1,3 @@
-<?php
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
- 
-///////////////////////////////////////
-// ツイート一覧
-///////////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2021-03-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました！',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-03-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-];
- 
-?>
 <!DOCTYPE html>
 <html lang="ja">
  
@@ -45,15 +10,14 @@ $view_tweets = [
 <body class="home">
     <div class="container">
         <?php include_once('../Views/common/side.php'); ?>
+ 
         <div class="main">
             <div class="main-header">
                 <h1>ホーム</h1>
             </div>
- 
-            <!-- つぶやき投稿エリア -->
             <div class="tweet-post">
                 <div class="my-icon">
-                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
+                    <img src="<?php echo htmlspecialchars($view_user['image_path']); ?>" alt="">
                 </div>
                 <div class="input-area">
                     <form action="post.php" method="post" enctype="multipart/form-data">
@@ -68,12 +32,10 @@ $view_tweets = [
                 </div>
             </div>
  
-            <!-- 仕切りエリア -->
             <div class="ditch"></div>
  
-            <!-- つぶやき一覧エリア -->
             <?php if (empty($view_tweets)) : ?>
-                <p class="p-3">ツイートがありません</p>
+                <p class="p-3">ツイートがまだありません</p>
             <?php else : ?>
                 <div class="tweet-list">
                     <?php foreach ($view_tweets as $view_tweet) : ?>
@@ -83,6 +45,7 @@ $view_tweets = [
             <?php endif; ?>
         </div>
     </div>
+ 
     <?php include_once('../Views/common/foot.php'); ?>
 </body>
  
