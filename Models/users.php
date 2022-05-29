c478484a2cd96da7e31705b70fe6b7338ad8a938
katayama hayato <>
2022-05-29 10:50:44 +0900

    各所修正

--- Models/users.php
+++ Models/users.php
@@ -2,7 +2,7 @@
 ///////////////////////////////////////
 // ユーザーデータを処理
 ///////////////////////////////////////
-
+ 
 /**
     * ユーザーを作成
     *
@@ -12,11 +12,6 @@
 function createUser(array $data)
 {
     // DB接続
-    // データベースの接続情報
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '8810');
-    define('DB_NAME', 'twitter_clone');
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
  
     // 接続エラーがある場合->処理停止
@@ -53,6 +48,67 @@ function createUser(array $data)
 }
  
 /**
+    * ユーザーを更新
+    *
+    * @param array $data
+    * @return bool
+    */
+function updateUser(array $data)
+{
+    // DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($mysqli->connect_errno) {
+        echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
+        exit;
+    }
+ 
+    // 更新日時を保存データに追加
+    $data['updated_at'] = date('Y-m-d H:i:s');
+ 
+    // パスワードがある場合->ハッシュ値に変換
+    if (isset($data['password'])) {
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+    }
+ 
+    // ------------------------------------
+    // SQLクエリを作成（更新）
+    // ------------------------------------
+    // SET句のカラムを準備
+    $set_columns = [];
+    foreach ([
+        'name', 'nickname', 'email', 'password', 'image_name', 'updated_at'
+    ] as $column) {
+        // 入力があれば、更新の対象にする
+        if (isset($data[$column]) && $data[$column] !== '') {
+            $set_columns[] = $column . ' = "' . $mysqli->real_escape_string($data[$column]) . '"';
+        }
+    }
+ 
+    // クエリ組み立て
+    $query = 'UPDATE users SET ' . join(',', $set_columns);
+    $query .= ' WHERE id = "' . $mysqli->real_escape_string($data['id']) . '"';
+ 
+    // ------------------------------------
+    // 戻り値を作成
+    // ------------------------------------
+    // クエリを実行
+    $response = $mysqli->query($query);
+ 
+    // SQLエラーの場合->エラー表示
+    if ($response === false) {
+        echo 'エラーメッセージ：' . $mysqli->error . "\n";
+    }
+ 
+    // ------------------------------------
+    // 後処理
+    // ------------------------------------
+    // DB接続を開放
+    $mysqli->close();
+ 
+    return $response;
+}
+ 
+/**
     * ユーザー情報取得：ログインチェック
     *
     * @param string $email
@@ -62,7 +118,6 @@ function createUser(array $data)
 function findUserAndCheckPassword(string $email, string $password)
 {
     // DB接続
-    // データベースの接続情報
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
  
     // 接続エラーがある場合->処理停止
@@ -70,6 +125,7 @@ function findUserAndCheckPassword(string $email, string $password)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
+ 
     // 入力値をエスケープ
     $email = $mysqli->real_escape_string($email);
  
@@ -79,21 +135,19 @@ function findUserAndCheckPassword(string $email, string $password)
  
     // クエリ実行
     $result = $mysqli->query($query);
-
+ 
     // クエリ実行に失敗した場合->return
     if (!$result) {
-        
         // MySQL処理中にエラー発生
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
         $mysqli->close();
         return false;
     }
-
+ 
     // ユーザー情報を取得
     $user = $result->fetch_array(MYSQLI_ASSOC);
     // ユーザーが存在しない場合->return
     if (!$user) {
-        
         $mysqli->close();
         return false;
     }
@@ -108,4 +162,70 @@ function findUserAndCheckPassword(string $email, string $password)
     $mysqli->close();
  
     return $user;
+}
+ 
+/**
+    * ユーザーを1件取得
+    *
+    * @param int $user_id
+    * @param int $login_user_id
+    * @return array|false
+    */
+function findUser(int $user_id, int $login_user_id = null)
+{
+    // DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($mysqli->connect_errno) {
+        echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
+        exit;
+    }
+ 
+    // エスケープ（SQLインジェクション対策）
+    $user_id = $mysqli->real_escape_string($user_id);
+    $login_user_id = $mysqli->real_escape_string($login_user_id);
+ 
+    // ------------------------------------
+    // SQLクエリを作成(検索)
+    // ------------------------------------
+    $query = <<<SQL
+        SELECT
+            U.id,
+            U.name,
+            U.nickname,
+            U.email,
+            U.image_name,
+            -- フォロー中の数
+            (SELECT COUNT(1) FROM follows WHERE status = 'active' AND follow_user_id = U.id) AS follow_user_count,
+            -- フォローワーの数
+            (SELECT COUNT(1) FROM follows WHERe status = 'active' AND followed_user_id = U.id) AS followed_user_count,
+            -- ログインユーザーがフォローしている場合、フォローIDが入る
+            F.id AS follow_id
+        FROM
+            users AS U
+            LEFT JOIN
+                follows AS F ON F.status = 'active' AND F.followed_user_id = '$user_id' AND F.follow_user_id = '$login_user_id'
+        WHERE
+            U.status = 'active' AND U.id = '$user_id'
+    SQL;
+ 
+    // ------------------------------------
+    // 戻り値を作成
+    // ------------------------------------
+    // クエリを実行し、SQLエラーでない場合
+    if ($result = $mysqli->query($query)) {
+        // 戻り値用の変数にセット：ユーザー情報1件
+        $response = $result->fetch_array(MYSQLI_ASSOC);
+    } else {
+        // 戻り値用の変数にセット：失敗
+        $response = false;
+        echo 'エラーメッセージ：' . $mysqli->error . "\n";
+    }
+ 
+    // ------------------------------------
+    // 後処理
+    // ------------------------------------
+    // DB接続を開放
+    $mysqli->close();
+ 
+    return $response;
 }
